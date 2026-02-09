ef4e8fc1fd73c47ff4fc3569c65b8321a9a79391
Mark Bernardo <markianstrong21@gmail.com>
2026-02-10 05:17:52 +0800

    another deployment error

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/programmer.png";
 import Particle from "../Particle";
 import Home2 from "./Home2";
